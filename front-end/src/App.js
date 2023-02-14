9529ed4179f3a0bc1271a4731eadaade20193f99
BautiOnorato <bautionorato@gmail.com>
2023-02-14 19:56:34 -0300

    Estructura basica

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from './Components/Layout';
+import { routes } from './Components/Utils/routes';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout/>}>
+          {routes.map(({ id, path, Element }) => (<Route key={id} path={path} element={<Element/>}/>))}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
