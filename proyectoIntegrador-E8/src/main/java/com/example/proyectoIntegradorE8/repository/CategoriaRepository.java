9a97622edf9a3098aa0dd57942bc3ea0e5877816
Francisco Gianzanti <francisco.gianzanti@gmail.com>
2023-02-20 18:29:59 +0000

    Revert "Merge branch 'back-end' into 'main'"
    This reverts merge request !3

--- proyectoIntegrador-E8/src/main/java/com/example/proyectoIntegradorE8/repository/CategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.proyectoIntegradorE8.repository;
-
-
-import com.example.proyectoIntegradorE8.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository <Categoria,Long> {
-}
