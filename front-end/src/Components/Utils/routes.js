d06bb85375b583ab111f4cd66b20aa3838d081cc
frangian <franciscogianzanti@gmail.com>
2023-02-20 15:25:32 -0300

    borre la carpeta front-end de la branch back-end

--- front-end/src/Components/Utils/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from "../../Rutas/Home";
-import Login from "../../Rutas/Login";
-import Register from "../../Rutas/Register";
-
-export const routes = [
-    { id: 1, path: "/", Element: Home, title: "Home" },
-    { id: 2, path: "/login", Element: Login, title: "login" },
-    { id: 3, path: "/register", Element: Register, title: "register" }, 
-]
