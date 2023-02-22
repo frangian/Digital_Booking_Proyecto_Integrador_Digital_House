eb4c005a0f39cb212449e0a48679e34cfbb17e99
Rodrigo <brause06@gmail.com>
2023-02-22 14:45:09 -0300

    Correcciones de estilos Rodrigo Brause

--- front-end/src/Components/CardRecomendaciones.jsx
+++ front-end/src/Components/CardRecomendaciones.jsx
@@ -1,31 +1,51 @@
 import * as React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPersonSwimming, faStar } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faWifi } from "@fortawesome/free-solid-svg-icons";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
-const CardRecomendaciones = ({ title, imagen, category, location, description }) => {
-  
+const CardRecomendaciones = ({
+  title,
+  imagen,
+  category,
+  location,
+  description,
+}) => {
   return (
     <div className="card-recomendaciones">
       <div className="image-container">
         <img src={imagen} alt={title} />
-        <FontAwesomeIcon icon={faHeart} className="fa-heart"/>
+        <FontAwesomeIcon icon={faHeart} className="fa-heart" />
       </div>
       <div className="info-container">
-        <div>
-          <div className="categoria-container">
-            <h3>{category}</h3>
-            <FontAwesomeIcon icon={faStar} className="icono icono-verde"/>
-            <FontAwesomeIcon icon={faStar} className="icono icono-verde"/>
-            <FontAwesomeIcon icon={faStar} className="icono icono-verde"/>
-            <FontAwesomeIcon icon={faStar} className="icono icono-verde"/>
+        <div className="info-container-header">
+          <div>
+            <div className="categoria-container">
+              <h3>{category}</h3>
+              <FontAwesomeIcon icon={faStar} className="icono icono-verde" />
+              <FontAwesomeIcon icon={faStar} className="icono icono-verde" />
+              <FontAwesomeIcon icon={faStar} className="icono icono-verde" />
+              <FontAwesomeIcon icon={faStar} className="icono icono-verde" />
+            </div>
+            <h2>{title}</h2>
           </div>
-          <h2>{title}</h2>
+          <div className="puntuacion-container">
+            <div className="square8">
+              <span id="square8-number">8</span>
+            </div>
+            <p>Muy bueno</p>
+          </div>
         </div>
-        <div className="ubicacion-container">
-          <FontAwesomeIcon icon={faMapMarkerAlt} className="icono" />
-          <p>{location} <span>MOSTRAR EN EL MAPA</span></p>
+        <div className="extras-container">
+          <div className="ubicacion-container">
+            <FontAwesomeIcon icon={faMapMarkerAlt} className="icono" />
+            <p>
+              {location} <span>MOSTRAR EN EL MAPA</span>
+            </p>
+          </div>
+          <FontAwesomeIcon icon={faWifi} className="icono extra" />
+          <FontAwesomeIcon icon={faPersonSwimming} className="icono extra" />
         </div>
         <p>{description}</p>
         <button className="ver-mas-btn">ver más</button>
