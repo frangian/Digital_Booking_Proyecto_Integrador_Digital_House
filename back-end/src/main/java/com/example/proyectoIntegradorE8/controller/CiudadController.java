b6fbc624aa45a60c347482f71a979106296a038b
frangian <franciscogianzanti@gmail.com>
2023-03-11 11:13:46 -0300

    v2 actualizacion de los metodos CRUD en Ciudad Service/Controller.

--- back-end/src/main/java/com/example/proyectoIntegradorE8/controller/CiudadController.java
+++ back-end/src/main/java/com/example/proyectoIntegradorE8/controller/CiudadController.java
@@ -8,28 +8,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.sql.SQLException;
 import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/ciudad")
 public class CiudadController {
-
     private static final Logger logger = Logger.getLogger(CiudadController.class);
-
     private CiudadService ciudadService;
-
     @Autowired
     public CiudadController (CiudadService ciudadService){
         this.ciudadService = ciudadService;
     }
 
     @PostMapping
-    public ResponseEntity<Ciudad> guardarCiudad (@RequestBody Ciudad ciudad) throws Exception {
-        logger.info("Se inicia el proceso para guardar una ciudad en la BBDD");
-        Ciudad ciudadGuardada = ciudadService.guardarCiudad(ciudad);
-        logger.info("La ciudad fue guardada en la BBDD exitosamente");
-        return ResponseEntity.ok(ciudadGuardada);
+    public ResponseEntity<?> guardarCiudad (@RequestBody Ciudad ciudad){
+        try {
+            logger.info("Se inicia el proceso para guardar una ciudad en la BBDD");
+            Ciudad ciudadGuardada = ciudadService.guardarCiudad(ciudad);
+            logger.info("La ciudad fue guardada "+ciudad.getNombre()+"   en la BBDD exitosamente");
+            return ResponseEntity.ok(ciudadGuardada);
+        } catch (SQLException bre){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bre.getMessage());
+        }
     }
     @GetMapping("/{id}")
     public ResponseEntity<?> buscarCiudad (@PathVariable Long id) {
@@ -44,29 +46,34 @@ public class CiudadController {
     public ResponseEntity<?> actualizarCiudad(@RequestBody Ciudad ciudad){
         try {
             ciudadService.buscarCiudad(ciudad.getId());
-            ciudadService.actualizarCiudad(ciudad);
+            ciudadService.guardarCiudad(ciudad);
             return ResponseEntity.ok(ciudad);
         } catch (ResourceNotFoundException rnfe){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(rnfe.getMessage());
+        } catch (SQLException sqle){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sqle.getMessage());
+        }
+    }
+    @GetMapping
+    public ResponseEntity<?> listarTodas(){
+        try {
+            List<Ciudad> ciudadesGuardadas = ciudadService.listarTodas();
+            logger.info("Mostrando todas las ciudades registradas en la BBDD");
+            return ResponseEntity.ok(ciudadesGuardadas);
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
-    @GetMapping
-    public ResponseEntity<List<Ciudad>> listarTodas(){
-        List<Ciudad> ciudadesGuardadas = ciudadService.listarTodas();
-        logger.info("Mostrando todas las ciudades registradas en la BBDD");
-        return ResponseEntity.ok(ciudadesGuardadas);
-    }
-
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> eliminarCiudad (@PathVariable Long id) {
+    public ResponseEntity<?> eliminarCiudad (@PathVariable Long id) {
         try {
             ciudadService.buscarCiudad(id);
             ciudadService.eliminarCiudad(id);
             return ResponseEntity.ok("Se elimino la ciudad con ID: "+id+" de la BBDD exitosamente");
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
--- back-end/src/main/java/com/example/proyectoIntegradorE8/service/CiudadService.java
+++ back-end/src/main/java/com/example/proyectoIntegradorE8/service/CiudadService.java
@@ -1,11 +1,13 @@
 package com.example.proyectoIntegradorE8.service;
 
 import com.example.proyectoIntegradorE8.entity.Ciudad;
+import com.example.proyectoIntegradorE8.exception.BadRequestException;
 import com.example.proyectoIntegradorE8.exception.ResourceNotFoundException;
 import com.example.proyectoIntegradorE8.repository.CiudadRepository;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,19 +15,18 @@ import java.util.Optional;
 public class CiudadService {
     private static final Logger logger = Logger.getLogger(CiudadService.class);
     private CiudadRepository ciudadRepository;
-
     @Autowired
     public CiudadService (CiudadRepository ciudadRepository){
         this.ciudadRepository = ciudadRepository;
     }
 
-    public Ciudad guardarCiudad (Ciudad ciudad) throws Exception {
+    public Ciudad guardarCiudad (Ciudad ciudad) throws SQLException {
         try {
-            logger.info("La informacion provista en el POST fue correcta, accediendo a CiudadRepository: "+ciudad.getNombre()+", "+ciudad.getPais());
+            logger.info("La informacion provista fue correcta, accediendo a CiudadRepository: "+ciudad.getNombre()+", "+ciudad.getPais());
             return ciudadRepository.save(ciudad);
         } catch (Exception e){
-            logger.error("No se pudo guardar la ciudad en la BBDD");
-            throw new Exception(e.getMessage());
+            logger.error("No se pudo guardar/actualizar la ciudad "+ciudad.getNombre()+" en la BBDD. Exception: "+e.getMessage()+".");
+            throw new SQLException("No se pudo guardar/actualizar la ciudad en la BBDD. No pueden quedar campos solicitados vacios.");
         }
     }
     public Ciudad buscarCiudad (Long id) throws ResourceNotFoundException {
@@ -39,29 +40,23 @@ public class CiudadService {
             throw new ResourceNotFoundException("La ciudad con id: "+id+" no existe en la BBDD");
         }
     }
-
-    public void actualizarCiudad (Ciudad ciudad) throws Exception {
-        try {
-            logger.info("Se inició una operación de actualizacion de la ciudad con id: "+ciudad.getId());
-            ciudadRepository.save(ciudad);
-        } catch (Exception e){
-            logger.info("No se pudo actualizar la ciudad");
-            throw new Exception(e.getMessage());
-        }
-    }
-    public List<Ciudad> listarTodas() {
+    public List<Ciudad> listarTodas() throws Exception {
         try {
             logger.info("Se inició una operación de listado de ciudades");
             return ciudadRepository.findAll();
         } catch (Exception e) {
-            logger.error("Error al listar las ciudades", e);
-            throw e;
+            logger.error("Error al listar las ciudades: Exception "+e.getMessage());
+            throw new BadRequestException("Ocurrio un error al listar todas las ciudades");
         }
     }
-
-    public void eliminarCiudad (Long id) {
-        ciudadRepository.deleteById(id);
-        logger.warn("Se eliminó la ciudad con ID: "+id+" de la BBDD");
+    public void eliminarCiudad (Long id) throws Exception {
+        try {
+            ciudadRepository.deleteById(id);
+            logger.warn("Se eliminó la ciudad con ID: "+id+" de la BBDD");
+        } catch (Exception e){
+            logger.error("Error al eliminar la ciudad: Exception "+e.getMessage());
+            throw new Exception("Ocurrio un error al eliminar la ciudad");
+        }
     }
 
 }
