60d58cc9ad2bbbe4a7320d13b95ece40ea35db18
frangian <franciscogianzanti@gmail.com>
2023-03-11 12:16:45 -0300

    Categoria: actualizacion de los metodos CRUD en Service/Controller y los Tests.

--- back-end/src/main/java/com/example/proyectoIntegradorE8/service/CategoriaService.java
+++ back-end/src/main/java/com/example/proyectoIntegradorE8/service/CategoriaService.java
@@ -1,59 +1,62 @@
 package com.example.proyectoIntegradorE8.service;
 
 import com.example.proyectoIntegradorE8.entity.Categoria;
+import com.example.proyectoIntegradorE8.exception.BadRequestException;
 import com.example.proyectoIntegradorE8.repository.CategoriaRepository;
 import com.example.proyectoIntegradorE8.exception.ResourceNotFoundException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CategoriaService {
-
     private static final Logger logger = Logger.getLogger(CategoriaService.class);
     CategoriaRepository categoriaRepository;
-
     @Autowired
     public CategoriaService (CategoriaRepository categoriaRepository){
         this.categoriaRepository = categoriaRepository;
     }
 
-    public Categoria guardarCategoria (Categoria categoria){
-        logger.info("Se inició una operación de guardado de las categorías con título: "+
-                categoria.getTitulo());
-        return categoriaRepository.save(categoria);
+    public Categoria guardarCategoria (Categoria categoria) throws SQLException {
+        try {
+            logger.info("La informacion provista fue correcta, accediendo a CategoriaRepository: "+categoria.getTitulo()+".");
+            return categoriaRepository.save(categoria);
+        } catch (Exception e){
+            logger.error("No se pudo guardar/actualizar la categoria "+categoria.getTitulo()+" en la BBDD. Exception: "+e.getMessage()+".");
+            throw new SQLException("No se pudo guardar/actualizar la categoria en la BBDD. No pueden quedar campos solicitados vacios.");
+        }
     }
-    public void actualizarCategoria (Categoria categoria){
-        logger.info("Se actualizó la categoría con id="+
-                categoria.getId());
-        categoriaRepository.save(categoria);
+    public Categoria buscarCategoria (Long id) throws ResourceNotFoundException {
+        logger.info("buscando categoria...");
+        Optional<Categoria> categoriaBuscada = categoriaRepository.findById(id);
+        if (categoriaBuscada.isPresent()) {
+            logger.info("Se encontro la categoria con id: " + id + " en la BBDD exitosamente");
+            return categoriaBuscada.get();
+        } else {
+            logger.info("La categoria con id: "+id+" no existe en la BBDD");
+            throw new ResourceNotFoundException("La categoria con id: "+id+" no existe en la BBDD");
+        }
     }
-
-    public Optional<Categoria> buscarCategoria (Long id){
-        logger.info("Se inició la búsqueda de la categoría con id="+id);
-        return categoriaRepository.findById(id);
+    public List<Categoria> listarCategorias () throws Exception {
+        try {
+            logger.info("Se inició una operación de listado de categorias");
+            return categoriaRepository.findAll();
+        } catch (Exception e) {
+            logger.error("Error al listar las categorias: Exception "+e.getMessage());
+            throw new BadRequestException("Ocurrio un error al listar todas las categorias");
+        }
     }
-
-    public void eliminarCategoria(Long id) throws ResourceNotFoundException {
-        Optional<Categoria> categoriaAEliminar = buscarCategoria(id);
-        if (categoriaAEliminar.isPresent()) {
+    public void eliminarCategoria(Long id) throws Exception {
+        try {
             categoriaRepository.deleteById(id);
-            logger.warn("Se realizo una operación de eliminar categoría con");
+            logger.warn("Se eliminó la categoria con ID: "+id+" de la BBDD");
+        } catch (Exception e){
+            logger.error("Error al eliminar la categoria: Exception "+e.getMessage());
+            throw new Exception("Ocurrio un error al eliminar la categoria");
         }
-        else{
-            throw new ResourceNotFoundException("La categoría a eliminar no existe en la base de datos");
-        }
-    }
-    public List<Categoria> buscarTodas (){
-        logger.info("Se inició una operación de listado de categorías");
-        return categoriaRepository.findAll();
     }
 
-//    public List<Categoria> randomHome (){
-//        logger.info("Se inició una operación de listado de 8 categorias random");
-//        return categoriaRepository.findAll();
-//    }
 }
