6c14ed809ca5ce0c1aeac4263d2f99960e85d113
frangian <franciscogianzanti@gmail.com>
2023-03-11 20:53:55 -0300

    Imagen: actualizacion de los metodos CRUD en Service/Controller.

--- back-end/src/main/java/com/example/proyectoIntegradorE8/controller/ImagenController.java
+++ back-end/src/main/java/com/example/proyectoIntegradorE8/controller/ImagenController.java
@@ -5,65 +5,84 @@ import com.example.proyectoIntegradorE8.exception.ResourceNotFoundException;
 import com.example.proyectoIntegradorE8.service.ImagenService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.sql.SQLException;
 import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/imagen")
 public class ImagenController {
-
     private static final Logger logger = Logger.getLogger(ImagenController.class);
-
     private ImagenService imagenService;
-
     @Autowired
     public ImagenController(ImagenService imagenService){
         this.imagenService = imagenService;
     }
 
     @PostMapping
-    public ResponseEntity<Imagen> guardarImagen (@RequestBody Imagen imagen) throws Exception {
-        Imagen imagenGuardada = imagenService.guardarImagen(imagen);
-        logger.info("Se registró la imagen: "+imagen.getTitulo()+", en la BBDD exitosamente");
-        return ResponseEntity.ok(imagenGuardada);
-    }
-    @PutMapping
-    public ResponseEntity<String> actualizarImagen(@RequestBody Imagen imagen) throws Exception {
-        Optional<Imagen> imagenBuscado = imagenService.buscarImagen(imagen.getId());
-        if(imagenBuscado.isPresent()){
-            imagenService.actualizarImagen(imagen);
-            return ResponseEntity.ok("Se actualizo la imagen: "+imagen.getTitulo()+", con ID: "+imagen.getId());
-        }
-        else {
-            throw new ResourceNotFoundException("No se puede actualizar los datos de la imagen con id: "+imagen.getId()+" porque no existe en la BBDD");
+    public ResponseEntity<?> guardarImagen (@RequestBody Imagen imagen) {
+        try {
+            logger.info("Se inicia el proceso para guardar una imagen en la BBDD");
+            Imagen imagenGuardada = imagenService.guardarImagen(imagen);
+            logger.info("La imagen "+imagen.getTitulo()+" fue guardada en la BBDD exitosamente");
+            return ResponseEntity.ok(imagenGuardada);
+        } catch (SQLException bre){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bre.getMessage());
         }
     }
     @GetMapping("/{id}")
-    public ResponseEntity<Imagen> buscarImagen (@PathVariable Long id) throws Exception {
-        Optional<Imagen> resultado = imagenService.buscarImagen(id);
-        if (resultado.isPresent()) {
-            logger.info("Se encontro la imagen con id "+id+" en la BBDD exitosamente");
-            return ResponseEntity.ok(resultado.get());
-        } else {
-            throw new ResourceNotFoundException("La imagen con id: "+id+" no existe en la BBDD");
+    public ResponseEntity<?> buscarImagen (@PathVariable Long id) {
+        try {
+            Imagen imagenBuscada = imagenService.buscarImagen(id);
+            return ResponseEntity.ok(imagenBuscada);
+        } catch (ResourceNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+    @PutMapping
+    public ResponseEntity<?> actualizarImagen(@RequestBody Imagen imagen){
+        try {
+            imagenService.buscarImagen(imagen.getId());
+            imagenService.guardarImagen(imagen);
+            return ResponseEntity.ok(imagen);
+        } catch (ResourceNotFoundException rnfe){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(rnfe.getMessage());
+        } catch (SQLException sqle){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sqle.getMessage());
         }
     }
     @GetMapping
-    public ResponseEntity<List<Imagen>> listarImagenes (){
-        return ResponseEntity.ok(imagenService.listarImagenes());
+    public ResponseEntity<?> listarImagenes (){
+        try {
+            List<Imagen> imagenesGuardadas = imagenService.listarImagenes();
+            logger.info("Mostrando todas las imagenes registradas en la BBDD");
+            return ResponseEntity.ok(imagenesGuardadas);
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminarImagen (@PathVariable Long id) {
+        try {
+            imagenService.buscarImagen(id);
+            imagenService.eliminarImagen(id);
+            return ResponseEntity.ok("Se elimino la imagen con ID: "+id+" de la BBDD exitosamente");
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @GetMapping("/producto/{producto}")
-    public ResponseEntity<List<Imagen>> productoPorCiudad(@PathVariable Long producto) {
+    public ResponseEntity<List<Imagen>> imagenesPorProducto(@PathVariable Long producto) {
         return ResponseEntity.ok(imagenService.imagenesPorProducto(producto));
     }
-
     @GetMapping("/categoria/{categoria}")
-    public ResponseEntity<List<Imagen>> imagenPorCategoria(@PathVariable Long categoria) {
+    public ResponseEntity<List<Imagen>> imagenesPorCategoria(@PathVariable Long categoria) {
         return ResponseEntity.ok(imagenService.imagenesPorCategoria(categoria));
     }
 }
