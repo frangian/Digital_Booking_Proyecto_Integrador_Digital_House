3ed0c4ec89df6ee13884c68a7ffc7f0922bf9e7c
frangian <franciscogianzanti@gmail.com>
2023-03-14 03:02:18 -0300

    Swagger UI integracion

--- back-end/src/main/java/com/example/proyectoIntegradorE8/ProyectoIntegradorE8Application.java
+++ back-end/src/main/java/com/example/proyectoIntegradorE8/ProyectoIntegradorE8Application.java
@@ -1,8 +1,11 @@
 package com.example.proyectoIntegradorE8;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info=@Info(title="Proyecto Integrador - Equipo 8", description = "Documentacion de la API del proyecto", version = "v1"))
 @SpringBootApplication
 public class ProyectoIntegradorE8Application {
 
