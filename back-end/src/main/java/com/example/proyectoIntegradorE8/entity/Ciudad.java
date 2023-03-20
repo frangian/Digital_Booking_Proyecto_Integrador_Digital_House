2ccb07f6f442d9270eb88a8c7a30635c52fece48
frangian <franciscogianzanti@gmail.com>
2023-03-20 16:46:21 -0300

    cambios de Set de List

--- back-end/src/main/java/com/example/proyectoIntegradorE8/entity/Ciudad.java
+++ back-end/src/main/java/com/example/proyectoIntegradorE8/entity/Ciudad.java
@@ -5,7 +5,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -25,7 +28,7 @@ public class Ciudad {
     private String pais;
     @OneToMany(mappedBy = "ciudad")
     @JsonIgnore
-    private Set<Producto> productos = new HashSet<>();
+    private List<Producto> productos = new ArrayList<>();
 
 //    public Ciudad() {
 //    }
