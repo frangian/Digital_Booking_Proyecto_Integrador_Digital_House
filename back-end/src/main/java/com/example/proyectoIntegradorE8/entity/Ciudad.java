67c1bcfde8c20ebca8d3a1f1a36fe8a0c110f775
frangian <franciscogianzanti@gmail.com>
2023-03-20 12:07:56 -0300

    Entidad Ciudad simplificada lombok

--- back-end/src/main/java/com/example/proyectoIntegradorE8/entity/Ciudad.java
+++ back-end/src/main/java/com/example/proyectoIntegradorE8/entity/Ciudad.java
@@ -2,10 +2,16 @@ package com.example.proyectoIntegradorE8.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table (name = "ciudad")
 public class Ciudad {
     @Id
@@ -21,70 +27,70 @@ public class Ciudad {
     @JsonIgnore
     private Set<Producto> productos = new HashSet<>();
 
-    public Ciudad() {
-    }
-
-    public Ciudad(Long id, String nombre, String provincia, String pais) {
-        this.id = id;
-        this.nombre = nombre;
-        this.provincia = provincia;
-        this.pais = pais;
-    }
-
-    public Ciudad(String nombre, String provincia, String pais) {
-        this.nombre = nombre;
-        this.provincia = provincia;
-        this.pais = pais;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getProvincia() {
-        return provincia;
-    }
-
-    public void setProvincia(String provincia) {
-        this.provincia = provincia;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    public Set<Producto> getProductos() {
-        return productos;
-    }
-
-    public void setProductos(Set<Producto> productos) {
-        this.productos = productos;
-    }
-
-    @Override
-    public String toString() {
-        return "Ciudad{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", provincia='" + provincia + '\'' +
-                ", pais='" + pais + '\'' +
-                ", productos=" + productos +
-                '}';
-    }
+//    public Ciudad() {
+//    }
+//
+//    public Ciudad(Long id, String nombre, String provincia, String pais) {
+//        this.id = id;
+//        this.nombre = nombre;
+//        this.provincia = provincia;
+//        this.pais = pais;
+//    }
+//
+//    public Ciudad(String nombre, String provincia, String pais) {
+//        this.nombre = nombre;
+//        this.provincia = provincia;
+//        this.pais = pais;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getNombre() {
+//        return nombre;
+//    }
+//
+//    public void setNombre(String nombre) {
+//        this.nombre = nombre;
+//    }
+//
+//    public String getProvincia() {
+//        return provincia;
+//    }
+//
+//    public void setProvincia(String provincia) {
+//        this.provincia = provincia;
+//    }
+//
+//    public String getPais() {
+//        return pais;
+//    }
+//
+//    public void setPais(String pais) {
+//        this.pais = pais;
+//    }
+//
+//    public Set<Producto> getProductos() {
+//        return productos;
+//    }
+//
+//    public void setProductos(Set<Producto> productos) {
+//        this.productos = productos;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Ciudad{" +
+//                "id=" + id +
+//                ", nombre='" + nombre + '\'' +
+//                ", provincia='" + provincia + '\'' +
+//                ", pais='" + pais + '\'' +
+//                ", productos=" + productos +
+//                '}';
+//    }
 }
