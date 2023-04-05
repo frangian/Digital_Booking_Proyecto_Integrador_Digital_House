51669faaca40a66d526712da5720e4575e5953db
Rodrigo <brause06@gmail.com>
2023-04-05 15:31:34 -0300

    quita de mas console log

--- front-end/src/Components/Utils/api.js
+++ front-end/src/Components/Utils/api.js
@@ -13,7 +13,6 @@ export const getProductosRecomendados = async (categoriaSeleccionada, ciudadSele
     // Filtro por categoría, ciudad y fechas
     const checkIn = dates[0];
     const checkOut = dates[1];
-    console.log("busco por categoria, ciudad y fechas" + dates);
     url = API_URL + `/producto/disponibles/categoriaciudadfecha`;
     params = {
       ciudadId: ciudadSeleccionada,
@@ -24,7 +23,6 @@ export const getProductosRecomendados = async (categoriaSeleccionada, ciudadSele
   } else if (categoriaSeleccionada && ciudadSeleccionada) {
     // Filtro por categoría y ciudad
     url = API_URL + `/producto/disponibles/categoriaciudad`;
-    console.log("Busco por categoria y ciudad");
     params = {
       ciudadId: ciudadSeleccionada,
       categoriaId: categoriaSeleccionada
@@ -33,8 +31,6 @@ export const getProductosRecomendados = async (categoriaSeleccionada, ciudadSele
     // Filtro por categoría y fechas
     const checkIn = dates[0];
     const checkOut = dates[1];
-    console.log("busco por categoria y fechas   " );
-    console.log(checkIn, checkOut, categoriaSeleccionada);
     url = API_URL + `/producto/disponibles/fechacategoria`;
     params = {
       categoriaId: categoriaSeleccionada,
@@ -44,12 +40,10 @@ export const getProductosRecomendados = async (categoriaSeleccionada, ciudadSele
   } else if (categoriaSeleccionada) {
     // Filtro por categoría
     url = API_URL + `/producto/categoria/${categoriaSeleccionada}`;
-    console.log("Busco por categoria");
   } else if (ciudadSeleccionada && dates && dates.length === 2) {
     // Filtro por ciudad y fechas
     const checkIn = dates[0];
     const checkOut = dates[1];
-    console.log("busco por ciudad y fechas");
     url = API_URL + `/producto/disponibles/fechaciudad`;
     params = {
       ciudadId: ciudadSeleccionada,
@@ -59,12 +53,10 @@ export const getProductosRecomendados = async (categoriaSeleccionada, ciudadSele
   } else if (ciudadSeleccionada) {
     // Filtro por ciudad
     url = API_URL + `/producto/ciudad/${ciudadSeleccionada}`;
-    console.log("Busco por ciudad");
   } else if (dates && dates.length === 2) {
     // Filtro por fechas
     const checkIn = dates[0];
     const checkOut = dates[1];
-    console.log("busco por fecha");
     url = API_URL + `/producto/disponibles/fecha`;
     params = {
       fechaInicial: checkIn,
@@ -77,7 +69,6 @@ export const getProductosRecomendados = async (categoriaSeleccionada, ciudadSele
 
   try {
     const response = await axios.get(finalUrl);
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error);
